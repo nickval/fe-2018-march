623ac02f4a1932287da22f116e261bf1c53ebb44
Nickval <nickval@ukr.net>
2018-03-18 18:58:10 +0200

    Changed while value

--- lection01/homework/main.js
+++ lection01/homework/main.js
@@ -1,9 +1,9 @@
 do {
-    var radius = prompt('Input cylinder radius', '');
-}while((radius < 1 || radius == null));
+    var radius = prompt('Input cylinder radius \n (value should be over the 0)', '');
+}while((radius < 1 || !radius));
 do {
-    var height = prompt('Input cylinder height', '');
-}while((height < 1 || height == null));
+    var height = prompt('Input cylinder height \n (value should be over the 0)', '');
+}while((height < 1 || !radius));
 square = Math.PI*Math.pow(radius,2);
 
 document.write('<p>**************</p>');
