13b37723eb8d13548f8b204347573ee5c28dec08
Nikolay Ignatyev <nikolay.ignatyev@progforce.com>
2018-03-23 15:09:02 +0200

    Added <tr> align

--- lection02/homework/table.js
+++ lection02/homework/table.js
@@ -2,7 +2,7 @@ document.write('<table border="1">');
 
 for (var i = 0; i <= 100; i++){
     if (i%2 == 0){
-        document.write('<tr><td>' + i + '</td></tr>');
+        document.write('<tr align="center"><td>' + i + '</td></tr>');
     }
 }
 
