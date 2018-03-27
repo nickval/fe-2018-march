4b8d40f2a683ba96b515a8d2e9efc392b0babdcb
Nikolay Ignatyev <nikolay.ignatyev@progforce.com>
2018-03-27 17:24:47 +0300

    Refactored rhomb variables

--- lection03/homework/js/rhomb.js
+++ lection03/homework/js/rhomb.js
@@ -1,23 +1,24 @@
 var h;
 
 do{
-    h = +prompt('Input h (half of diagonal) (it should be an odd number):',);
-}while(isNaN(h) || h <= 0 || h%2 == 0);
+    h = +prompt('Input h (diagonal) (it should be an even number):',);
+}while(isNaN(h) || h <= 0 || h%2 != 0);
 
 var star = '*';
 var space = '&nbsp';
+var mid = h / 2;
 
-for (var i = 1; i < h * 2; i++){
-    for (var j = 1; j < h * 2; j++){
-        if (i <= h){
-            if ((j == h - i + 1) || (j == h + i - 1)) {
+for (var i = 1; i < h; i++){
+    for (var j = 1; j < h; j++){
+        if (i <= mid){
+            if ((j == mid - i + 1) || (j == mid + i - 1)) {
                 document.write(star);
             } else {
                 document.write(space);
             }
         // }
         } else {
-            if ((j == i - h + 1) || (j == 2* h - (i - h + 1))) {
+            if ((j == i - mid + 1) || (j == h - (i - mid + 1))) {
                 document.write(star);
             } else {
                 document.write(space);
