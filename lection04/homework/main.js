e67c1cfff08c6e1295bd52140e7d91a779545465
Nickval <nickval@ukr.net>
2018-03-28 20:22:20 +0300

    Refactored code

--- lection04/homework/main.js
+++ lection04/homework/main.js
@@ -1,12 +1,12 @@
 do {
     var n = +prompt();
-}while(isNaN(n) || n <= 0);
+} while (isNaN(n) || n <= 0);
 
 var A = new Array(n);
-var B = new Array;
-var min, max, posMax, posMin;
 
-for (i = 0; i < n; i++) {
+//Task 1. Создать массив А размерностью n. Заполнить случайными числами любом диапазоне. 
+
+for (let i = 0; i < n; i++) {
     A[i] = Math.floor(Math.random() * 999);
 }
 
@@ -17,14 +17,28 @@ for (i = 0; i < n; i++) {
 // A[2] = 16;
 // A[3] = 16;
 
-console.log('Random nubers array: ',A);
+console.log('Random numbers array: ', A);
 
+//Task 1. i.  проверить все числа на простоту, и найденные простые числа сохранить в массив B.
+//        ii. найти максимальное число и минимальное число. 
+
+var B = [];
+var min, max, posMax, posMin;
 min = A[0];
 posMin = 0;
 max = A[0];
 posMax = 0;
 
-for (i = 0; i < n; i++) {
+for (let i = 0; i < n; i++) {
+    for (let j = 2; j <= A[i]; j++) {
+        if (A[i] % j == 0) {
+            if (j < A[i]) {
+                break;
+            } else if (j == A[i]) {
+                B[B.length] = A[i];
+            }
+        }
+    }
     if (max < A[i]) {
         max = A[i];
         posMax = i;
@@ -33,45 +47,38 @@ for (i = 0; i < n; i++) {
         min = A[i];
         posMin = i;
     }
-    for (j = 2; j <= A[i]; j++) {
-        if (A[i]%j == 0 && j < A[i]) {
-            break;
-        } else if (A[i]%j == 0 && j == A[i]){
-            B[B.length] = A[i];
-        } else {
-            continue;
-        }
-    }
 }
 
-console.log('max = ' + max + '; min = ' + min);
-// console.log(posMax, posMin)
 console.log('Prime numbers array: ', B);
+console.log('max = ' + max + ' (pos: ' + posMax + '); min = ' + min + ' (pos: ' + posMin + ')');
 
-//Task 2
+//Task 2. Элементы массива между min -- max записать в массив B. Массив взять из 1го задания
 
-var C = new Array();
+var C = [];
 if (posMin < posMax) {
-    for (var i = posMin; i <= posMax; i++){
+    for (let i = posMin; i <= posMax; i++) {
         C[C.length] = A[i];
     }
 } else {
-    for (var i = posMax; i <= posMin; i++){
+    for (let i = posMax; i <= posMin; i++) {
         C[C.length] = A[i];
     }
 }
 
 console.log('Array between min and max: ', C);
 
-//Task 3
+//Task 3. В массиве A поменять местами min и max. Массив взять из 2го задания
+
 var tmp = A[posMin];
 A[posMin] = A[posMax];
 A[posMax] = tmp;
 
 console.log('Array with replaced min and max: ', A);
 
-//Task 4. Revese array
-for (var i = 0; i <= A.length/2; i++) {
+//Task 4. Перевернуть массив, т.е. если был массив 1, 5, 6, 2, 4 -- то мы должны получить 4, 2, 6, 5, 1. 
+//        Нельзя использовать стандартный метод reverse().
+
+for (let i = 0; i <= A.length / 2; i++) {
     tmp = A[i];
     A[i] = A[A.length - 1 - i];
     A[A.length - 1 - i] = tmp;
@@ -79,10 +86,11 @@ for (var i = 0; i <= A.length/2; i++) {
 
 console.log('Reversed Array: ', A);
 
-//Task 5
+//Task 5. Определить количество элементов в заданном массиве, отличающихся от минимального на 5.
+
 var numberElements = 0;
-for (i = 0; i < A.length; i++) {
-    if (A[i] == min +5) {
+for (let i = 0; i < A.length; i++) {
+    if (A[i] == min + 5) {
         numberElements++;
     }
 }
